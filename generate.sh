e34bcb832d58b60a66981fbf045094e5a59ff6a8
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2016-06-17 16:52:32 -0700

    Include visitor in generated code

--- generate.sh
+++ generate.sh
@@ -3,6 +3,6 @@
 set -x
 set -e
 
-antlr4 -Dlanguage=Java -package com.signalfx.signalflow.grammar -o java/src/main/java/com/signalfx/signalflow grammar/*.g4
-antlr4 -Dlanguage=JavaScript -package signalflow.grammar -o javascript/lib grammar/*.g4
-antlr4 -Dlanguage=Python2 -package signalflow.grammar -o python/signalflow grammar/*.g4
+antlr4 -Dlanguage=Java -visitor -package com.signalfx.signalflow.grammar -o java/src/main/java/com/signalfx/signalflow grammar/*.g4
+antlr4 -Dlanguage=JavaScript -visitor -package signalflow.grammar -o javascript/lib grammar/*.g4
+antlr4 -Dlanguage=Python2 -visitor -package signalflow.grammar -o python/signalflow grammar/*.g4
