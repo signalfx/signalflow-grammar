signalfx/signalflow-grammar

commit 2325aebaaac74ab3dd9011701ac89a08fa37f373

Support for parsing function definitions
Language parsing now supports python definitions (There is no
support for using them yet)

In order to do this "native" code need to be added to the
Lexer. Unfortunately this means the parser and lexer files needed
to be split. The only client visible side effect of this is that
the SignalFlowV2Visitor is now called SignalFlowV2ParserVisitor.

Also in keeping with the official python.g4, statement was
renamed to exprStatement (as this is really what it was)

Cleaned up generate.sh to reduce C+P code and also added
"make" like steps for each port to check for compilation issues

grammar/SignalFlowV2Parser.g4
@@ -1,0 +1,143 @@
+/**
+ * Copyright (C) 2016 SignalFx, Inc.
+ */
+parser grammar SignalFlowV2Parser;
+
+options { tokenVocab = SignalFlowV2Lexer; }
+tokens { INDENT, DEDENT }
+
+program
+  : ( NEWLINE | statement )* EOF
+  ;
+
+functionDefinition
+ : DEF ID parameters ':' suite
+ ;
+
+parameters
+ : OPEN_PAREN varArgsList? CLOSE_PAREN
+ ;
+
+varArgsList
+ : varArgsListParamDef ( ',' varArgsListParamDef )*
+ ;
+
+varArgsListParamDef
+ : varArgsListParamName ( '=' test)?
+ ;
+
+varArgsListParamName
+ : ID
+ ;
+
+statement
+  : simpleStatement
+  | compoundStatement
+  ;
+
+simpleStatement
+  :  smallStatement ( ';' smallStatement )* ';'? NEWLINE?
+  ;
+
+smallStatement
+  : exprStatement
+  | returnStatement
+  ;
+
+exprStatement
+  : (ID BINDING)? test
+  ;
+
+returnStatement
+ : RETURN test?
+ ;
+
+flowStatment
+ : returnStatement
+ ;
+
+compoundStatement
+  : functionDefinition
+  ;
+
+suite
+ : simpleStatement
+ | NEWLINE INDENT statement+ DEDENT
+ ;
+
+test
+  : or_test (IF or_test ELSE test)?
+  | lambdef
+  ;
+
+lambdef
+  : LAMBDA ID COLON test
+  ;
+
+or_test
+  : and_test (OR and_test)*
+  ;
+
+and_test
+  : not_test (AND not_test)*
+  ;
+
+not_test
+  : NOT not_test
+  | comparison
+  ;
+
+comparison
+  : expr ((LT | LE | EQ | NE | GT | GE) expr)*
+  ;
+
+expr
+  : term ((PLUS | MINUS) term)*
+  ;
+
+term
+  : factor ((MUL | DIV) factor)*
+  ;
+
+factor
+  : (PLUS | MINUS) factor
+  | power
+  ;
+
+power
+  : atom_expr (POW factor)?
+  ;
+
+atom_expr
+  : atom trailer*
+  ;
+
+atom
+  : list_value
+  | OPEN_PAREN test CLOSE_PAREN
+  | ID
+  | INT
+  | FLOAT
+  | STRING+
+  | NONE
+  | TRUE
+  | FALSE
+  ;
+
+list_value
+  : LSQUARE (test (COMMA test)*)? RSQUARE
+  ;
+
+trailer
+  : OPEN_PAREN actual_args? CLOSE_PAREN
+  | DOT ID
+  ;
+
+actual_args
+  : argument (COMMA argument)*
+  ;
+
+argument
+  : (ID BINDING)? test
+  ;
+

.gitignore
@@ -7,4 +7,5 @@
 /python/build/
 /python/dist/
 /python/*.egg-info
+/grammar/SignalFlowV2Lexer.g4
 *.pyc

generate.sh
@@ -2,19 +2,37 @@
 
 set -e
 
-GRAMMAR="grammar/SignalFlowV2.g4"
+PARSER_GRAMMAR="grammar/SignalFlowV2Parser.g4"
+LEXER_GRAMMAR="grammar/SignalFlowV2Lexer.g4"
+
+function run_antlr() {
+  local lang=$1;
+  local antlr_lang=$2;
+  local libdir=$3;
+  local package=$4;
+  local output=$5;
+  local delpattern=${6:-*};
+
+  echo "${lang}/"
+  echo "  - generating sources from ${PARSER_GRAMMAR} and ${LEXER_GRAMMAR}..."
+  ./generate_grammar ${LEXER_GRAMMAR} ${lang}
+  mkdir -p ${libdir}
+  rm -rf ${libdir}/${delpattern};
+  antlr4 -Dlanguage=${antlr_lang} -visitor -lib ${libdir} -package ${package} \
+   -o ${output} ${PARSER_GRAMMAR} ${LEXER_GRAMMAR}
+  rm ${LEXER_GRAMMAR}
+}
 
 function do_java() {
   local version=$1
 
-  echo "java/"
-  echo "  - generating sources from ${GRAMMAR}..."
-  antlr4 -Dlanguage=Java -visitor -package com.signalfx.signalflow.grammar \
-    -o java/src/main/java/com/signalfx/signalflow ${GRAMMAR}
+  run_antlr java Java java/src/main/java/com/signalfx/signalflow/grammar \
+    com.signalfx.signalflow.grammar java/src/main/java/com/signalfx/signalflow
 
   echo "  - updating pom version to ${version}..."
   cd java
   mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${version} > /dev/null
+  mvn compile
   cd ..
 }
 
@@ -22,10 +40,9 @@ function do_javascript() {
   local version=$1
   local bundle="signalflow-grammar-${version}.js"
 
-  echo "javascript/"
-  echo "  - generating javascript sources from ${GRAMMAR}..."
-  antlr4 -Dlanguage=JavaScript -visitor -package signalflow.grammar \
-    -o javascript/lib ${GRAMMAR}
+  run_antlr javascript JavaScript javascript/lib/grammar \
+    signalflow.grammar javascript/lib \
+    com.signalfx.signalflow.grammar java/src/main/java/com/signalfx/signalflow
 
   echo "  - updating node.js package version to ${version}..."
   cd javascript
@@ -39,14 +56,13 @@ function do_javascript() {
 function do_python() {
   local version=$1
 
-  echo "python/"
-  echo "  - generating python sources from ${GRAMMAR}..."
-  antlr4 -Dlanguage=Python2 -visitor -package signalflow.grammar \
-    -o python/signalflow ${GRAMMAR}
+  run_antlr python Python2 python/signalflow/grammar \
+    signalflow.grammar python/signalflow 'S*'
 
   echo "  - updating python package version to ${version}..."
   cd python
   sed -i "" -e "s/version='.*'/version='${version}'/" setup.py
+  python setup.py build
   cd ..
 }
 

generate_grammar
@@ -1,0 +1,16 @@
+#!/usr/bin/env python
+
+# Generates SignalFlowLexer.g4 file from template file and "include" files
+import os
+import sys
+
+out_path = sys.argv[1]
+lang = sys.argv[2]
+
+tmpl = open(out_path + ".tmpl", 'r').read()
+tmpl = tmpl.replace('###LEXER_MEMBERS###', open(os.path.join(lang, 'grammar', 'lexer_members.g4')).read())
+tmpl = tmpl.replace('###NEWLINE###', open(os.path.join(lang, 'grammar', 'newline.g4')).read())
+tmpl = tmpl.replace('###PARENS###', open(os.path.join(lang, 'grammar', 'parens.g4')).read())
+out_f = open(out_path, 'w')
+out_f.write(tmpl)
+out_f.close()

grammar/SignalFlowV2.g4
@@ -1,262 +1,0 @@
-/**
- * Copyright (C) 2016 SignalFx, Inc.
- */
-grammar SignalFlowV2;
-
-program
-  : interior_line* last_line
-  ;
-
-interior_line
-  : statement? NEWLINE
-  ;
-
-last_line
-  : statement? NEWLINE? EOF
-  ;
-
-statement
-  : (ID BINDING)? test
-  ;
-
-test
-  : or_test (IF or_test ELSE test)?
-  | lambdef
-  ;
-
-lambdef
-  : LAMBDA ID COLON test
-  ;
-
-or_test
-  : and_test (OR and_test)*
-  ;
-
-and_test
-  : not_test (AND not_test)*
-  ;
-
-not_test
-  : NOT not_test
-  | comparison
-  ;
-
-comparison
-  : expr ((LT | LE | EQ | NE | GT | GE) expr)*
-  ;
-
-expr
-  : term ((PLUS | MINUS) term)*
-  ;
-
-term
-  : factor ((MUL | DIV) factor)*
-  ;
-
-factor
-  : (PLUS | MINUS) factor
-  | power
-  ;
-
-power
-  : atom_expr (POW factor)?
-  ;
-
-atom_expr
-  : atom trailer*
-  ;
-
-atom
-  : list_value
-  | LPAREN test RPAREN
-  | ID
-  | INT
-  | FLOAT
-  | STRING+
-  | NONE
-  | TRUE
-  | FALSE
-  ;
-
-list_value
-  : LSQUARE (test (COMMA test)*)? RSQUARE
-  ;
-
-trailer
-  : LPAREN actual_args? RPAREN
-  | DOT ID
-  ;
-
-actual_args
-  : argument (COMMA argument)*
-  ;
-
-argument
-  : (ID BINDING)? test
-  ;
-
-LAMBDA
-  : 'lambda'
-  ;
-
-NONE
-  : 'None'
-  ;
-
-TRUE
-  : 'True'
-  ;
-
-FALSE
-  : 'False'
-  ;
-
-IF
-  : 'if'
-  ;
-
-ELSE
-  : 'else'
-  ;
-
-INT
-  : [0-9]+
-  ;
-
-FLOAT
-  : [0-9]* '.'? [0-9]+ ([eE][+-]?[0-9]+)?
-  ;
-
-STRING
-  : '\'' (ESCAPE_SEQ | ~( '\'' | '\\' | '\r' | '\n'))* '\''
-  | '"' (ESCAPE_SEQ | ~( '"' | '\\' | '\r' | '\n'))* '"'
-  ;
-
-fragment
-ESCAPE_SEQ
-  : '\\' ([btnfr] | '\"' | '\'' | '\\' | [0-3][0-7][0-7] | [0-7][0-7] | [0-7])
-  ;
-
-LE
-  : '<='
-  ;
-
-GE
-  : '>='
-  ;
-
-EQ
-  : '=='
-  ;
-
-NE
-  : '!='
-  ;
-
-LT
-  : '<'
-  ;
-
-GT
-  : '>'
-  ;
-
-OR
-  : 'or'
-  ;
-
-AND
-  : 'and'
-  ;
-
-NOT
-  : 'not'
-  ;
-
-ID
-  : [a-zA-Z_][a-zA-Z0-9_]*
-  ;
-
-QMARK
-  : '?'
-  ;
-
-EXCL
-  : '!'
-  ;
-
-LPAREN
-  : '('
-  ;
-
-RPAREN
-  : ')'
-  ;
-
-LSQUARE
-  : '['
-  ;
-
-RSQUARE
-  : ']'
-  ;
-
-LBRACE
-  : '{'
-  ;
-
-RBRACE
-  : '}'
-  ;
-
-COMMA
-  : ','
-  ;
-
-SEMICOLON
-  : ';'
-  ;
-
-COLON
-  : ':'
-  ;
-
-PLUS
-  : '+'
-  ;
-
-MINUS
-  : '-'
-  ;
-
-MUL
-  : '*'
-  ;
-
-DIV
-  : '/'
-  ;
-
-POW
-  : '**'
-  ;
-
-BINDING
-  : '='
-  ;
-
-DOT
-  : '.'
-  ;
-
-NEWLINE
-  : '\n'
-  ;
-
-WS
-  : [ \t\r]+ -> skip
-  ;
-
-LINE_COMMENT
-  : '#' ~[\r\n]* -> skip
-  ;

grammar/SignalFlowV2Lexer.g4.tmpl
@@ -1,0 +1,180 @@
+/**
+ * Copyright (C) 2016 SignalFx, Inc.
+ */
+lexer grammar SignalFlowV2Lexer;
+
+###LEXER_MEMBERS###
+DEF
+  : 'def'
+  ;
+
+RETURN
+  : 'return'
+  ;
+
+LAMBDA
+  : 'lambda'
+  ;
+
+NONE
+  : 'None'
+  ;
+
+TRUE
+  : 'True'
+  ;
+
+FALSE
+  : 'False'
+  ;
+
+IF
+  : 'if'
+  ;
+
+ELSE
+  : 'else'
+  ;
+
+INT
+  : [0-9]+
+  ;
+
+FLOAT
+  : [0-9]* '.'? [0-9]+ ([eE][+-]?[0-9]+)?
+  ;
+
+STRING
+  : '\'' (ESCAPE_SEQ | ~( '\'' | '\\' | '\r' | '\n'))* '\''
+  | '"' (ESCAPE_SEQ | ~( '"' | '\\' | '\r' | '\n'))* '"'
+  ;
+
+fragment
+ESCAPE_SEQ
+  : '\\' ([btnfr] | '\"' | '\'' | '\\' | [0-3][0-7][0-7] | [0-7][0-7] | [0-7])
+  ;
+
+LE
+  : '<='
+  ;
+
+GE
+  : '>='
+  ;
+
+EQ
+  : '=='
+  ;
+
+NE
+  : '!='
+  ;
+
+LT
+  : '<'
+  ;
+
+GT
+  : '>'
+  ;
+
+OR
+  : 'or'
+  ;
+
+AND
+  : 'and'
+  ;
+
+NOT
+  : 'not'
+  ;
+
+ID
+  : [a-zA-Z_][a-zA-Z0-9_]*
+  ;
+
+QMARK
+  : '?'
+  ;
+
+EXCL
+  : '!'
+  ;
+
+###PARENS###
+
+LSQUARE
+  : '['
+  ;
+
+RSQUARE
+  : ']'
+  ;
+
+LBRACE
+  : '{'
+  ;
+
+RBRACE
+  : '}'
+  ;
+
+COMMA
+  : ','
+  ;
+
+SEMICOLON
+  : ';'
+  ;
+
+COLON
+  : ':'
+  ;
+
+PLUS
+  : '+'
+  ;
+
+MINUS
+  : '-'
+  ;
+
+MUL
+  : '*'
+  ;
+
+DIV
+  : '/'
+  ;
+
+POW
+  : '**'
+  ;
+
+BINDING
+  : '='
+  ;
+
+DOT
+  : '.'
+  ;
+
+###NEWLINE###
+
+fragment SPACES
+ : [ \t]+
+ ;
+
+SKIP_
+ : ( SPACES | COMMENT | LINE_JOINING ) -> skip
+ ;
+ 
+COMMENT
+  : '#' ~[\r\n]*
+   ;
+
+fragment LINE_JOINING
+ : '\\' SPACES? ( '\r'? '\n' | '\r' )
+ ;
+

java/grammar/lexer_members.g4
@@ -1,0 +1,92 @@
+/**
+ * Copyright (C) 2016 SignalFx, Inc.
+ */
+@lexer::members {
+  // A queue where extra tokens are pushed on (see the NEWLINE lexer rule).
+  private java.util.LinkedList<Token> tokens = new java.util.LinkedList<Token>();
+  // The stack that keeps track of the indentation level.
+  private java.util.Stack<Integer> indents = new java.util.Stack<Integer>();
+  // The amount of opened braces, brackets and parenthesis.
+  private int opened = 0;
+  // The most recently produced token.
+  private Token lastToken = null;
+  @Override
+  public void emit(Token t) {
+    super.setToken(t);
+    tokens.offer(t);
+  }
+
+  @Override
+  public Token nextToken() {
+    // Check if the end-of-file is ahead and there are still some DEDENTS expected.
+    if (_input.LA(1) == EOF && !this.indents.isEmpty()) {
+      // Remove any trailing EOF tokens from our buffer.
+      for (int i = tokens.size() - 1; i >= 0; i--) {
+        if (tokens.get(i).getType() == EOF) {
+          tokens.remove(i);
+        }
+      }
+
+      // First emit an extra line break that serves as the end of the statement.
+      this.emit(commonToken(SignalFlowV2Parser.NEWLINE, "\n"));
+
+      // Now emit as much DEDENT tokens as needed.
+      while (!indents.isEmpty()) {
+        this.emit(createDedent());
+        indents.pop();
+      }
+
+      // Put the EOF back on the token stream.
+      this.emit(commonToken(SignalFlowV2Parser.EOF, "<EOF>"));
+    }
+
+    Token next = super.nextToken();
+
+    if (next.getChannel() == Token.DEFAULT_CHANNEL) {
+      // Keep track of the last token on the default channel.
+      this.lastToken = next;
+    }
+
+    return tokens.isEmpty() ? next : tokens.poll();
+  }
+
+  private Token createDedent() {
+    CommonToken dedent = commonToken(SignalFlowV2Parser.DEDENT, "");
+    dedent.setLine(this.lastToken.getLine());
+    return dedent;
+  }
+
+  private CommonToken commonToken(int type, String text) {
+    int stop = this.getCharIndex() - 1;
+    int start = text.isEmpty() ? stop : stop - text.length() + 1;
+    return new CommonToken(this._tokenFactorySourcePair, type, DEFAULT_TOKEN_CHANNEL, start, stop);
+  }
+
+  // Calculates the indentation of the provided spaces, taking the
+  // following rules into account:
+  //
+  // "Tabs are replaced (from left to right) by one to eight spaces
+  //  such that the total number of characters up to and including
+  //  the replacement is a multiple of eight [...]"
+  //
+  //  -- https://docs.python.org/3.1/reference/lexical_analysis.html#indentation
+  static int getIndentationCount(String spaces) {
+    int count = 0;
+    for (char ch : spaces.toCharArray()) {
+      switch (ch) {
+        case '\t':
+          count += 8 - (count % 8);
+          break;
+        default:
+          // A normal space char.
+          count++;
+      }
+    }
+
+    return count;
+  }
+
+  boolean atStartOfInput() {
+    return super.getCharPositionInLine() == 0 && super.getLine() == 1;
+  }
+}

java/grammar/newline.g4
@@ -1,0 +1,39 @@
+/**
+ * Copyright (C) 2016 SignalFx, Inc.
+ */
+NEWLINE
+ : ( {atStartOfInput()}?   SPACES
+   | ( '\r'? '\n' | '\r' ) SPACES?
+   )
+   {
+     String newLine = getText().replaceAll("[^\r\n]+", "");
+     String spaces = getText().replaceAll("[\r\n]+", "");
+     int next = _input.LA(1);
+     if (opened > 0 || next == '\r' || next == '\n' || next == '#') {
+       // If we're inside a list or on a blank line, ignore all indents, 
+       // dedents and line breaks.
+       skip();
+     }
+     else {
+       emit(commonToken(NEWLINE, newLine));
+       int indent = getIndentationCount(spaces);
+       int previous = indents.isEmpty() ? 0 : indents.peek();
+
+       if (indent == previous) {
+         // skip indents of the same size as the present indent-size
+         skip();
+       }
+       else if (indent > previous) {
+         indents.push(indent);
+         emit(commonToken(SignalFlowV2Parser.INDENT, spaces));
+       }
+       else {
+         // Possibly emit more than 1 DEDENT token.
+         while(!indents.isEmpty() && indents.peek() > indent) {
+           this.emit(createDedent());
+           indents.pop();
+         }
+       }
+     }
+   }
+ ;

commit 724022bf9a44f77fae590e67dced65709ee982cb

Fix small_statement so a newline is required between them
Fixes a bug introduced in 2325aebaaac, which didn't require
small_statements to be separated by newlines. Now small_statements
must be separated by newline or be terminated by an EOF

grammar/SignalFlowV2Parser.g4
@@ -40,7 +40,7 @@ statement
   ;
 
 simple_statement
-  :  small_statement ( ';' small_statement )* ';'? NEWLINE?
+  :  small_statement ( ';' small_statement )* ';'? (NEWLINE | EOF)
   ;
 
 small_statement